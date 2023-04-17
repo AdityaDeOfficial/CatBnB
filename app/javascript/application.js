fa9fdc92476515072d288cbbfaa562619e380428
Philip Curry <muttaphilly@gmail.com>
2023-04-17 20:11:27 +0800

    Geocoding added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,8 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+// fixes for map_controller.js error
+// ran yarn add mapbox-gl
+import mapboxgl from 'mapbox-gl';
+// remove above To revert
+// run yarn remove mapbox-gl

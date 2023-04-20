6f480376b61a5100c110432e4af9bbfdcec359d1
Philip Curry <muttaphilly@gmail.com>
2023-04-20 19:34:54 +0800

    mapbox popup styling

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,7 +27,7 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      const popup = new mapboxgl.Popup({className: "clear-popup"}).setHTML(marker.info_window_html)
       const customMarker = document.createElement("div")
       customMarker.innerHTML = marker.marker_html
       new mapboxgl.Marker(customMarker)

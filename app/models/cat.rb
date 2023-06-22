7e7d582e77f4bf29893a624fbb53f5e4b8f0c798
Mary Chan <marychancfy@gmail.com>
2023-06-22 20:59:56 +1000

    general set up of image upload is done

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,5 @@ class Cat < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many_attached :photos #trace: this probably gonna cause error 22jun2023
 end

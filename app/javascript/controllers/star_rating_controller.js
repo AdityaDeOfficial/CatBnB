d55830219cf1f1387da80047272a27a6aeb3fcff
Philip Curry <muttaphilly@gmail.com>
2023-07-02 13:07:51 +0800

    customisable JS options added

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -4,6 +4,16 @@ import StarRating from "star-rating.js"
 // Connects to data-controller="star-rating"
 export default class extends Controller {
   connect() {
-    new StarRating(this.element)
+    new StarRating(this.element), {
+        classNames: {
+          active: "gl-active",
+          base: "gl-star-rating",
+          selected: "gl-selected",
+        },
+        clearable: true,
+        maxStars: 5,
+        stars: null,
+        tooltip: 'Select a Rating',
+      }
+    }
   }
-}

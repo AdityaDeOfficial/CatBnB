23d8bb20f711accefc3b4dc6bada6dada1f217c0
Philip Curry <muttaphilly@gmail.com>
2023-07-02 08:47:56 +0800

    bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -43,16 +43,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_102049) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "cat_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.integer "price"
+    t.integer "total_price"
     t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["cat_id"], name: "index_bookings_on_cat_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "cats", force: :cascade do |t|
@@ -91,6 +90,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_102049) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "cats"
-  add_foreign_key "bookings", "users"
   add_foreign_key "cats", "users"
 end
